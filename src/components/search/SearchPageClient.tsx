937e34a042a721d8427c37a1b6c03f7962b2da81
Do Thuy Van <dothuyvan0805@gmail.com>
2026-03-29 17:37:18 +0000

    breadcrumb

--- src/components/search/SearchPageClient.tsx
+++ src/components/search/SearchPageClient.tsx
@@ -5,6 +5,7 @@ import { useSearchParams, useRouter } from "next/navigation";
 import Link from "next/link";
 import { Search } from "lucide-react";
 
+
 import { type Product } from "~/types/product";
 import {
   Breadcrumb,
