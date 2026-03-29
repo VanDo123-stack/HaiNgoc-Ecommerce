3ecadf1aa4ddc6a47bf72dd134c6595b6a135ac5
Do Thuy Van <dothuyvan0805@gmail.com>
2026-03-29 17:38:31 +0000

    root

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -3,3 +3,4 @@ import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
 export const appRouter = createTRPCRouter({});
 export type AppRouter = typeof appRouter;
 export const createCaller = createCallerFactory(appRouter);
+
